c291baa56feb13dc63545ed8e73b75df306987a1
Bisma Khan <bismaashfaq009@gmail.com>
2024-11-13 00:22:34 +0500

    feat: Configure user model and authentication

--- event_management/users/forms.py
+++ event_management/users/forms.py
@@ -1,6 +1,8 @@
 from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from django import forms
 from django.contrib.auth import forms as admin_forms
+from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 from .models import User
@@ -24,12 +26,41 @@ class UserAdminCreationForm(admin_forms.UserCreationForm):
         }
 
 
+# Define a validator for Pakistani mobile numbers
+pakistani_mobile_validator = RegexValidator(
+    regex=r"^(?:\+92|0)3\d{9}$",
+    message="Enter a valid Pakistani mobile number, e.g., +923362108399 or 03362108399",
+)
+
+
 class UserSignupForm(SignupForm):
     """
     Form that will be rendered on a user sign up section/screen.
     Default fields will be added automatically.
     Check UserSocialSignupForm for accounts created from social.
     """
+
+    mobile_number = forms.CharField(
+        max_length=15,
+        required=True,
+        label="Mobile Number",
+        validators=[pakistani_mobile_validator],  # Add the validator here
+        widget=forms.TextInput(
+            attrs={"placeholder": "Mobile Number"},
+        ),  # Add placeholder here
+    )
+
+    def save(self, request):
+        # Ensure you call the parent class's save.
+        # .save() returns a User object.
+        user = super().save(request)
+
+        # Add your own processing here.
+        user.mobile_number = self.cleaned_data["mobile_number"]
+        user.save()
+
+        # You must return the original result.
+        return user
 
 
 class UserSocialSignupForm(SocialSignupForm):
--- event_management/users/models.py
+++ event_management/users/models.py
@@ -13,6 +13,7 @@ class User(AbstractUser):
 
     # First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    mobile_number = CharField(_("Mobile Number"), blank=True, max_length=13)
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
 
